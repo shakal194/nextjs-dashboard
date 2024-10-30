8dffbb724b7f326630d97b00f44a32acef54e63a
Oleksandr Soloviov <shakal194@gmail.com>
2024-10-30 20:24:41 +0200

    Update LoadingSpinnerRobinAPITest.tsx

--- app/ui/_components/LoadingSpinnerRobinAPITest.tsx
+++ app/ui/_components/LoadingSpinnerRobinAPITest.tsx
@@ -1,17 +1,41 @@
+'use client';
+
 import { Spinner } from '@nextui-org/spinner';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
 
 interface LoadingSpinnerRobinAPITest {
   size?: 'sm' | 'md' | 'lg';
-  color?: 'white' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger';
+  color?: 'white' | 'primary';
 }
 
 const LoadingSpinnerRobinAPITest: React.FC<LoadingSpinnerRobinAPITest> = ({
   size = 'sm',
-  color = 'white',
 }) => {
+  const { theme, resolvedTheme } = useTheme();
+  const [spinnerColor, setSpinnerColor] = useState<'primary' | 'white'>(
+    'primary',
+  );
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      setSpinnerColor(resolvedTheme === 'light' ? 'primary' : 'white');
+    }
+  }, [resolvedTheme]);
+
+  if (!mounted) {
+    // Пока компонент не смонтирован, возвращаем null
+    return null;
+  }
+
   return (
-    <div className="ml-4 flex items-center justify-center bg-black bg-opacity-50">
-      <Spinner size={size} color={color} />
+    <div className="ml-4 flex items-center justify-center bg-opacity-50">
+      <Spinner size={size} color={spinnerColor} />
     </div>
   );
 };
