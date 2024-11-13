bba2b42d39052685079626843ddbfe985dd8e4f7
Oleksandr Soloviov <shakal194@gmail.com>
2024-11-13 14:25:32 +0200

    Update LoadingSpinner.tsx

--- app/ui/_components/LoadingSpinner.tsx
+++ app/ui/_components/LoadingSpinner.tsx
@@ -1,17 +1,39 @@
-import { Spinner } from '@nextui-org/spinner';
+'use client';
 
-interface LoadingSpinnerProps {
+import { Spinner } from '@nextui-org/spinner';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+interface LoadingSpinner {
   size?: 'sm' | 'md' | 'lg';
-  color?: 'white' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger';
+  color?: 'white' | 'primary';
 }
 
-const LoadingSpinner: React.FC<LoadingSpinnerProps> = ({
-  size = 'lg',
-  color = 'white',
-}) => {
+const LoadingSpinner: React.FC<LoadingSpinner> = ({ size = 'lg' }) => {
+  const { theme, resolvedTheme } = useTheme();
+  const [spinnerColor, setSpinnerColor] = useState<'primary' | 'white'>(
+    'primary',
+  );
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      setSpinnerColor(resolvedTheme === 'light' ? 'primary' : 'white');
+    }
+  }, [resolvedTheme]);
+
+  if (!mounted) {
+    // Пока компонент не смонтирован, возвращаем null
+    return null;
+  }
+
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <Spinner size={size} color={color} label="Loading..." />
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-slate-200 bg-opacity-50">
+      <Spinner size={size} label="Loading..." color={spinnerColor} />
     </div>
   );
 };
